0f81abb09af055f49fef743bc60b77703c74fd8e
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-10-05 14:40:51 +1100

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
+const { response } = require('express');
 app.use(cors());
 
 // Initialize the main project folder
@@ -24,3 +25,20 @@ app.use(express.static('website'));
 // Setup Server
 const port = 3000;
 app.listen(port, () => {console.log(`running on the local host: ${port}`)})
+
+//Receive the request from the client and save data to the endpoint object
+app.post('/post', receiveData)
+
+function receiveData(request, response) {
+    let data = request.body;
+    projectData["temp"] = data.temperature;
+    projectData["date"] = data.date;
+    projectData["feeling"] = data.userResponse;
+    response.send(projectData);
+    console.log(projectData);
+}
+
+// Make GET request
+app.get('/get',(request, response)=>{
+    response.send(projectData);
+})
